a080d727075771d895d5c3b6d7ff7e931abe8d8e
Gabriel Gebril <gebril.gabriel@gmail.com>
2020-11-13 05:22:17 -0500

    Declared state variables that will be shared between developers. Spun up a stub calendar to illustrate the concept.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  // Need the state to be stored up here so others can use it to change the behaviour of thier 
+  // components.
+  const [notify, toggleNotification] = useState(true); //use this for the message component to see if you need to send a notification or not.
+  const [keybinds, onBindSave] = useState([{}]);
+  const [events, onEventSave] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
